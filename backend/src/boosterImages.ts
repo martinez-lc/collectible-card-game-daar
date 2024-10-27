9e069b5ff1cd634a43046a34f94cf45cea6c3c5c
martinez-lc <114682463+martinez-lc@users.noreply.github.com>
2024-10-28 00:45:33 +0100

    Delete backend/src/boosterImages.ts

--- backend/src/boosterImages.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const img = [
-    {img: "https://images.pokemontcg.io/hgss4/1.png", gid: 239 },
-    {img: "https://images.pokemontcg.io/hgss4/1.png", gid: 121},
-    {img: "https://images.pokemontcg.io/hgss4/1.png", gid: 73},
-    {img: "hhttps://images.pokemontcg.io/hgss4/1.png", gid: 128}
-]
